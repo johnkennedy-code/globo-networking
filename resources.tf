740a13cb2a84debbc65e20a43aa0e23d24956682
John Kennedy <j.kennedy.feebee@gmail.com>
2024-10-25 22:46:28 -0400

    Add third subnet and tags (#1)

--- resources.tf
+++ resources.tf
@@ -17,7 +17,8 @@ data "aws_availability_zones" "available" {}
 ##################################################################################
 locals {
   common_tags = {
-    environment = var.environment
+    Environment = var.environment
+    BillingCode = var.billing_code
   }
 }
 
